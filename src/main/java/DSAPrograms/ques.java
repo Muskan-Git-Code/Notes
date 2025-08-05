bfc38cb594b6d6bcbfa7731634156ba99650397c
Muskan <muskan18179@gmail.com>
2025-08-05 11:57:15 +0530

    Basics commit

--- src/main/java/DSAPrograms/ques.java
+++ src/main/java/DSAPrograms/ques.java
@@ -4,29 +4,9 @@ import java.util.*;
 
 public class ques {
 
-    public int subarrayBitwiseORs(int[] arr) {
-        int n= arr.length;
-        HashSet<Integer> set= new HashSet<>();
-
-        for(int i=0; i<n; i++){
-            int ct=0;
-            for(int j=i; j<n; j++){
-                ct|= arr[i];
-                set.add(ct);
-
-
-                System.out.println(ct+" "+ arr[i]);
-            }
-            System.out.println();
-        }
-        return set.size();
-    }
-
     public static void main(String args[]){
         ques q= new ques();
         int arr[]= {1,1,2};
-
-        q.subarrayBitwiseORs(arr);
 
     }
 
